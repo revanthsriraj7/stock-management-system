465aca69641bc2d1652b79f3dc17c87b94f5fef6
revanthsriraj7 <146690516+revanthsriraj7@users.noreply.github.com>
2023-11-03 09:20:06 +0530

    wifi code esp

---
+++ wifi_code.ino
@@ -0,0 +1,27 @@
+//wifi code
+//9:10 am
+//3/11/23
+//code 5
+
+#define WIFI_SSID "wifi SSID"         //defining wifi username and passowrod to be connected
+#define WIFI_PASSWORD "wifi password"
+
+void setup() {
+  Serial.println("Connecting to Wi-Fi...");
+   while (WiFi.status() != WL_CONNECTED)
+   {
+       Serial.println("Not connected to the internet");
+       WiFi.begin(WIFI_SSID, WIFI_PASSWORD);              //conecting to wifi
+   }
+   if (WiFi.status() == WL_CONNECTED)
+    {
+        digitalWrite(LED_BUILTIN, HIGH);
+        Serial.println("Connected to the internet");
+    }
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
